7c50e394a8569efe8e2e56b33412288a183b1bab
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-06-19 21:23:24 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            Task<int> t = Task.Run<int>(() =>
+            {
+                return AddNumber(tokenSource);
+            }).ContinueWith<int>((Task<int> papa) =>
+            {
+                Console.WriteLine(papa.Result);
+                return papa.Result;
+            });
+            Console.ReadLine();
+            tokenSource.Cancel();
+            //Console.WriteLine(t.Result);
+        }
+       
+        private static int AddNumber(CancellationTokenSource tokenSource)
+        {
+            int sum = 0;
+            int i = 1;
+            while (i < 5 && !tokenSource.IsCancellationRequested)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+                sum = sum + i;
+                i++;
+            }
+            tokenSource.Token.ThrowIfCancellationRequested();
+            return sum;
+        }
+       
+    }
+}
